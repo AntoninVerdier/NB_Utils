985cbd13a0aa81ceb6cc0c7c642b5311a3103616
Antonin Verdier <antonin.verdier@laposte.net>
2022-08-03 14:25:33 +0200

    save electrophy and add multi cpu

---
+++ Electrophy/Recording.py
@@ -0,0 +1,283 @@
+import os
+import time
+import numpy as np 
+import pandas as pd
+import pickle as pkl
+import scipy.io as sio
+import multiprocessing as m
+import settings as s
+
+from rich.progress import track
+
+import matplotlib.pyplot as plt
+
+params = s.params()
+class Recording():
+	"""docstring for Recording"""
+	def __init__(self, ksdir, sound_info, name=None, fs=20000):
+		self.fs = fs
+		self.name = name
+		self.ksdir = ksdir
+		self.sound_path = sound_info
+		self.date = 0 # To define
+		self.idxs_clu = None
+		self.output = os.path.join(self.ksdir, 'output')
+		if not os.path.exists(self.output):
+			os.makedirs(self.output)
+		self.__load_data()
+
+	def __add__(self, other, n_pres=15):
+		selfkeys = [k for k in self.d_stims]
+		otherkeys = [k for k in other.d_stims]
+		common_keys = np.intersect1d(selfkeys, otherkeys)
+
+		new_dstism = {}
+		for sound in common_keys:
+			new_dstism[sound] ={}
+			for p in range(n_pres):
+				new_dstism[sound][p] = self.d_stims[sound][p] + other.d_stims[sound][p]
+		self.d_stims = new_dstism
+
+		return self
+
+	def __load_data(self):
+		# Load files from KS directory
+		sp_times = np.load(os.path.join(self.ksdir,'spike_times.npy'))
+		self.sp_times = np.array([i[0] for i in sp_times])
+
+		self.sp_clu = np.load(os.path.join(self.ksdir,'spike_clusters.npy'))
+		
+		clu_gp = pd.read_csv(os.path.join(self.ksdir,'cluster_group.tsv'), delimiter='\t')
+		self.clu_gp = clu_gp['group'].to_numpy()
+
+		# Be careful with 1-indexing of matlab so remove 1
+		sound_info = sio.loadmat(self.sound_path)
+		self.s_vector = np.squeeze(sound_info['StimsVector']) - 1
+		self.sd_names = np.array([n[0] for n in np.squeeze(sound_info['SoundNames'])])
+
+		f = open(os.path.join(self.ksdir, 'digitalin.dat'), 'rb') 
+		sd_array = np.fromfile(f, np.int16)
+		f.close()
+		self.ttl_idxs = np.where(np.insert(np.diff(sd_array), 0, 0) == 1)[0]
+		print(self.s_vector.shape)
+
+		# Not necessary to load for PSTHs
+		# spikes_templates = np.load(os.path.join(self.ksdir,'spike_templates.npy'))
+		# templates = np.load(os.path.join(self.ksdir,'templates.npy'))
+		# spikes_amplitude = np.load(os.path.join(self.ksdir, 'amplitudes.npy'))
+		# winv = np.load(os.path.join(self.ksdir,'whitening_mat_inv.npy'))
+		# coords = np.load(os.path.join(self.ksdir,'channel_positions.npy'))
+	
+	def select_data_quality(self, quality='good'):
+		""" Extract soundnames and remove unwanted cluster categories"""
+
+		# Get cluster numbers that are not noise
+		if quality == 'good':
+			usable_clusters = [i for i, clu in enumerate(self.clu_gp) if clu == 'good']
+		elif quality =='mua':
+			usable_clusters = [i for i, clu in enumerate(self.clu_gp) if not clu == 'noise']
+		elif quality =='noise':
+			usable_clusters = list(range(len(self.clu_gp)))
+
+		# Clean spike timings to keep only good clusters
+		self.usable_sp = np.isin(self.sp_clu, usable_clusters)
+		self.sp_times = self.sp_times[self.usable_sp]
+		self.sp_clu = self.sp_clu[self.usable_sp]
+
+	# def new_ttl_alignment(self):
+	# 	idxs_clu = []
+	# 	for clu in np.unique(self.sp_clu):
+	# 		idxs_clu.append(np.where(self.sp_clu == clu)[0])
+	# 	d_stim = {k:{i: [ for idx in idxs_clu]
+	# 			  for i, ttl in enumerate(self.ttl_idxs[np.where(self.s_vector == k)[0]])}
+	# 			  for k in np.unique(self.s_vector)}
+	# 	print(d_stim)
+
+	def __get_idxs_clu(self):
+		self.idxs_clu = []
+		for clu in np.unique(self.sp_clu):
+			self.idxs_clu.append(np.where(self.sp_clu == clu)[0])
+		self.n_clu = len(self.idxs_clu)
+
+
+	def __ttl_alignment_single_cpu(self, pad_before, pad_after):
+		if not self.idxs_clu:
+			self.__get_idxs_clu()
+
+		# Structure of array is dict[stim_number][presentation_number] containg a number_of_cluster * spikes matrix
+		self.d_stims = {}
+		for stim in track(np.unique(self.s_vector), description='Aligning neural activity to ttls ...'):
+			stim_ttls = self.ttl_idxs[np.where(self.s_vector == stim)[0]]
+			self.d_stims[stim] = {}
+			for i, ttl in enumerate(stim_ttls):
+				spikes = self.sp_times[self.sp_times > ttl + pad_before]
+				spikes = np.array(spikes[spikes < ttl + pad_after], dtype=np.int64)
+
+				self.d_stims[stim][i] = [(np.intersect1d(self.sp_times[idx_clu], spikes) - ttl)*1000/params.fs for idx_clu in self.idxs_clu]
+
+	def ttl_alignment_multi(self, args):
+		""" in dev
+		"""
+		if not self.idxs_clu:
+			self.__get_idxs_clu()
+		# Structure of array is dict[stim_number][presentation_number] containg a number_of_cluster * spikes matrix
+		stim_ttls = self.ttl_idxs[np.where(self.s_vector == args[0])[0]]
+		self.d_stims[args[0]] = {}
+		for i, ttl in enumerate(stim_ttls):
+			spikes = self.sp_times[self.sp_times > ttl + args[1]]
+			spikes = np.array(spikes[spikes < ttl + args[2]], dtype=np.int64)
+			self.d_stims[args[0]][i] = [(np.intersect1d(self.sp_times[idx_clu], spikes) - ttl)*1000/params.fs for idx_clu in self.idxs_clu]
+
+	def ttl_alignment(self, pad_before=0, pad_after=20000, multi=True):
+		if os.path.isfile(os.path.join(self.output, '{}_aligned_spikes.pkl'.format(self.name))):
+			self.d_stims = pkl.load(open(os.path.join(self.output, '{}_aligned_spikes.pkl'.format(self.name)), 'rb'))
+		else:
+			self.d_stims = {}
+			if multi:
+				args = ([stim, pad_before, pad_after] for stim in track(np.unique(self.s_vector)))
+				with m.Pool() as pool:
+					pool.map(self.ttl_alignment_multi, args)
+			else:
+				self.__ttl_alignment_single_cpu(pad_before, pad_after)
+
+			pkl.dump(self.d_stims, open(os.path.join(self.output, '{}_aligned_spikes.pkl'.format(self.name)), 'wb'))
+
+
+	def get_population_vectors(self, pad_before, pad_after, normalize=True):
+		pop_vectors = {}
+		for stim in self.d_stims:
+			pop_vectors[stim] = {}
+			for pres in self.d_stims[stim]:
+				pop_vector = []
+				for i, clu in enumerate(self.d_stims[stim][pres]):
+					sp = clu[clu > pad_before]
+					sp = np.array(sp[sp < pad_after], dtype=np.int64)
+					pop_vector.append(sp.shape[0])
+				pop_vectors[stim][pres] = pop_vector
+
+		self.pop_vectors = pop_vectors
+
+		return pop_vectors
+
+	def get_timings_vectors(self, pad_before, pad_after, timebin=10):
+		time_vectors = {}
+		bins = list(np.arange(pad_before, pad_after + 10, timebin))
+		for stim in self.d_stims:
+			time_vectors[stim] = {}
+			for pres in self.d_stims[stim]:
+				time_vector = np.sort([s for clu in self.d_stims[stim][pres] for s in clu])
+				time_vector = time_vector[time_vector > - pad_before]
+				time_vector = np.array(time_vector[time_vector < pad_after], dtype=np.int64)
+				time = np.histogram(time_vector, bins)[0]
+
+				time_vectors[stim][pres] = time
+
+		self.time_vectors = time_vectors
+
+		return time_vectors
+
+	def get_complete_vectors(self, pad_before, pad_after, timebin, normalize=True):
+		bins = np.arange(0, 1000, timebin)
+		pop_vectors = {}
+		for stim in self.d_stims:
+			pop_vectors[stim] = {}
+			for pres in self.d_stims[stim]:
+				pop_vector = []
+				for i, clu in enumerate(self.d_stims[stim][pres]):
+					clu = clu[clu > - pad_before]
+					clu = np.array(clu[clu < pad_after], dtype=np.int64)
+					pvec = np.histogram(clu, bins)[0]
+					if normalize:
+						pvec = np.nan_to_num((pvec - np.min(pvec))/np.max(pvec))
+					pop_vector.append(pvec)
+				pop_vectors[stim][pres] = np.array(pop_vector).flatten()
+
+		self.pop_vectors = pop_vectors
+
+		return pop_vectors
+	def __compute_svm(kernel='linear'):
+		clf = svm.SVC()
+		########## Need to make sure that data is shuffled
+		scores = cross_val_score(clf, X, y, cv=5)
+		return scores
+
+	def svm_preformance(rec, task=params.task1, scale=np.arange(50, 1000, 50)):
+		"""define all y_task
+		"""
+		scores = []
+		for p in track(scale, description='Compute SVM ...'):
+			pop_vectors = rec.get_population_vectors(0, p)
+
+			X = np.array([pop_vectors[stim][p] for stim in task for p in pop_vectors[stim]])
+			
+			if i < 2:
+				y = np.array([0 if i < 8 else 1 for i, stim in enumerate(task) for p in pop_vectors[stim]])
+			elif i == 2:
+				y = params.y_task3
+			elif i == 3:
+				y = params.y_task4
+
+			score = compute_svm(X, y)
+			scores.append([np.mean(score), np.std(score)])
+
+		scores = np.array(scores).reshape(-1, 2)
+
+		plt.errorbar(scale, scores[:, 0], label='Task {}'.format(i + 1))
+
+		plt.legend()
+		plt.savefig('performance_svm_timings.png')
+		plt.show()
+
+	def raster_plot(self):
+		activity = {}
+		bins = np.arange(0, 1000, 10)
+		for stim in self.d_stims:
+			activity[stim] = [np.sort(np.concatenate([self.d_stims[stim][pres][i] for pres in self.d_stims[stim]])) for i in range(len(self.d_stims[stim][0]))]
+
+		for stim in activity:
+			raster = [np.histogram(clu, bins)[0] for clu in activity[stim]]
+			print(np.max(raster), np.min(raster))
+			plt.imshow(raster, cmap='binary', vmin=0, vmax=np.max(raster)/3)
+			plt.savefig('Output/Raster/raster_{}.svg'.format(params.sound_names[stim]), dpi=300)
+			plt.close()
+
+		self.activity = activity
+
+		return activity
+
+
+
+
+
+
+
+
+
+'''
+Extend tasks to tasks 3 and 4
+Concatenate across dimensions to get large array
+Make proper figures from it (svm performance model, save for each task
+make an __add__ method to merge two recording together
+add metadat to recording object ? So I can save it for later
+'''
+
+	# def get_pop_vectors(self, pad_before=params.pad_before, pad_after=params.pad_after, task=params.task1):
+	# 	idxs_clu = []
+	# 	for j, clu in enumerate(np.unique(self.sp_clu)):
+	# 		idxs_clu.append(np.where(self.sp_clu == clu)[0])
+
+	# 	# Structure of array is dict[stim_number][presentation_number] containg a number_of_cluster * spikes matrix
+	# 	d_stims = {}
+	# 	for stim in track(np.unique(self.s_vector), description='Generating individual timings ...'):
+	# 		stim_ttls = self.ttl_idxs[np.where(self.s_vector == stim)[0]]
+	# 		d_stims[stim] = {}
+	# 		for i, ttl in enumerate(stim_ttls):
+	# 			spikes = self.sp_times[self.sp_times > ttl - params.pad_before]
+	# 			spikes = np.array(spikes[spikes < ttl + pad_after], dtype=np.int64)
+	# 			print(len(idxs_clu[i]))
+	# 			print(self.sp_times[idxs_clu[i]], spikes)
+
+	# 			d_stims[stim][i] = [len(np.intersect1d(self.sp_times[idx_clu], spikes) - ttl)/(params.fs*1000) for idx_clu in idxs_clu]
+
+
+	# 	self.pop_vectors = [d_stims[k] for k in task]
---
+++ Electrophy/main.py
@@ -0,0 +1,276 @@
+import os
+import umap
+import scipy.io as sio
+import numpy as np
+import h5py
+
+import matplotlib
+from rich import print
+from rich.progress import track
+from rich.traceback import install 
+import matplotlib.pyplot as plt
+import pandas as pd 
+import seaborn as sns
+
+from sklearn import svm
+from sklearn.model_selection import train_test_split, cross_val_score, LeaveOneOut, RepeatedKFold, cross_val_predict
+from sklearn.manifold import TSNE
+from sklearn.decomposition import PCA
+from sklearn.metrics import confusion_matrix
+from sklearn.utils import shuffle
+
+
+
+import settings as s
+from Recording import Recording
+
+paths = s.paths()
+params = s.params()
+
+def compute_svm(X, y):
+
+    scores = []
+    X, y = shuffle(X, y)
+    clf = svm.SVC(kernel='linear')
+    scores = cross_val_score(clf, X, y, cv=5)
+
+
+    ########## Need to make sure that data is shuffled
+    #scores = cross_val_score(clf, X, y, cv=5)
+    return scores
+
+def svm_preformance(rec):
+    for i, t in enumerate([params.task1, params.task2, params.task3, params.task4]):
+        scores = []
+        for p in track(np.arange(0, 1000, 50), description='Compute SVM for each task ...'):
+            pop_vectors = rec.complete_vectors(0, p)
+
+            X = np.array([pop_vectors[stim][p] for stim in t for p in pop_vectors[stim]])
+            if i < 2:
+                y = np.array([0 if i < 8 else 1 for i, stim in enumerate(t) for p in pop_vectors[stim]])
+            elif i == 2:
+                y = params.y_task3
+            elif i == 3:
+                y = params.y_task4
+
+            score = compute_svm(X, y)
+            scores.append([np.mean(score), np.std(score)])
+
+        scores = np.array(scores).reshape(-1, 2)
+
+        plt.errorbar(np.arange(0, 1000, 50), scores[:, 0], label='Task {}'.format(i + 1))
+
+    plt.legend()
+    plt.savefig('performance_svm_population.png')
+    plt.show()
+
+def psychocurve(rec, p=1000, timebin=10):
+    colors = ['blue', 'forestgreen']
+    for tidx, t in enumerate([params.task1, params.task2]):
+        pop_vectors = rec.get_complete_vectors(0, p, timebin=timebin)
+
+        X = np.array([pop_vectors[stim][p] for stim in t for p in pop_vectors[stim]])
+        y = np.array([0 if i < 8 else 1 for i, stim in enumerate(t) for p in pop_vectors[stim]])
+        true_classes = np.array([i for i, stim in enumerate(t) for p in pop_vectors[stim]]) + 1
+
+        #X, y, true_classes = shuffle(X, y, true_classes)
+        psycos = []
+        for train_index, test_index in track(LeaveOneOut().split(X), total=X.shape[0]):
+            clf = svm.SVC(kernel='linear')
+            X_train, X_test = X[train_index], X[test_index]
+            y_train, y_test = y[train_index], y[test_index]
+            tc_train, tc_test = true_classes[train_index], true_classes[test_index]
+
+            clf.fit(X_train, y_train)
+            y_pred = clf.predict(X_test)
+            correct = np.logical_not(np.logical_xor(y_pred, y_test))
+            counting_vec = list(correct * tc_test)
+
+            for i in np.unique(true_classes):
+                try:
+                    if i < 9:
+                        psycos.append(counting_vec.count(i)/list(tc_test).count(i))
+                    else:
+                        psycos.append(1 - counting_vec.count(i)/list(tc_test).count(i))
+                except ZeroDivisionError:
+                    psycos.append(0)
+
+        psycos = np.array(psycos).reshape(X.shape[0], 16)
+        psycos = np.mean(psycos, axis=0)
+
+        plt.plot(np.geomspace(20, 200, 16), psycos, color=colors[tidx], linewidth=2, markersize=6, marker='o')
+        plt.xscale('log')
+        plt.savefig('Output/TimeAverage/Task{}_win_{}_bin_{}ms'.format(tidx + 1, p, timebin), dpi=300)
+        
+        plt.close()
+
+
+if __name__ == '__name__':
+    recs = []
+    main_folder = '/home/anverdie/Documents/Electrophy/To_analyze'
+    for folder in os.listdir(main_folder):
+        cp = os.path.join(main_folder, folder)
+        print('Analyzing {} ...'.format(folder))
+        rec = Recording(cp, os.path.join(cp, 'SoundInfo.mat'), name=folder)
+        rec.select_data_quality(quality='good')
+        rec.ttl_alignment(multi=False)
+        recs.append(rec)
+
+
+    rec = np.sum(recs)
+    #rec.raster_plot()
+    #rec.raster_plot()
+
+    #svm_preformance(rec)
+    # for j in track(range(40, 80, 5)):
+    #     for i in np.arange(10, j + 5, 5):
+    psychocurve(rec)
+
+
+    pop_vectors = rec.get_population_vectors(0, 1000)
+
+    cmap = matplotlib.cm.get_cmap('hsv')
+    colors = []
+    for stim in [s for s in np.unique(rec.s_vector) for p in pop_vectors[s]]:
+        if stim in params.task1:
+            colors.append(cmap(0.1)) # Orange
+        elif stim in params.task2:
+            colors.append(cmap(0.3)) # vert
+        elif stim in params.task3:
+            colors.append(cmap(0.5)) # Blue
+        elif stim in params.task4:
+            colors.append(cmap(0.7))  #ble dark
+        else:
+            colors.append(cmap(0.9)) # Pink
+
+
+
+    # stims = [s for s in np.unique(rec.s_vector) for p in pop_vectors[s]]
+    # colors, values = [], []
+    # for stim in stims:
+    #   if stim in params.task1:
+    #       cmap = matplotlib.cm.get_cmap('Blues')
+    #       value = np.where(stim == params.task1)[0][0]/len(params.task1)
+    #   elif stim in params.task2:
+    #       cmap = matplotlib.cm.get_cmap('Reds')
+    #       value = np.where(stim == params.task2)[0][0]/len(params.task2)
+    #   elif stim in params.task3:
+    #       cmap = matplotlib.cm.get_cmap('Greens')
+    #       value = np.where(stim == params.task3)[0][0]/len(params.task3)
+    #   elif stim in params.task4:
+    #       cmap = matplotlib.cm.get_cmap('Greys')
+    #       value = np.where(stim == params.task4)[0][0]/len(params.task4)
+    #   else:
+    #       value = (1, 1, 1, 1)
+
+    #   colors.append(cmap(value))
+    #   print(len(colors))
+
+
+    for time in track(range(50, 1050, 50)):
+        pop_vectors = rec.get_population_vectors(0, time)
+        X = np.array([pop_vectors[stim][p] for stim in np.unique(rec.s_vector) for p in pop_vectors[stim]])
+
+        tsne = TSNE(n_components=2)
+        Y = tsne.fit_transform(X)
+
+        plt.scatter(Y[:, 0], Y[:, 1], c=colors)
+
+
+        plt.savefig('Output/TSNE/tsne_time_{}.png'.format(time))
+        plt.close()
+
+    X = np.array([pop_vectors[stim][p] for stim in np.unique(rec.s_vector) for p in pop_vectors[stim]])
+
+    umap = umap.UMAP(n_neighbors=5)
+    Y = umap.fit_transform(X)
+
+    print(X.shape, Y.shape)
+    plt.scatter(Y[green, 0], Y[green, 1], c="g")
+    plt.scatter(Y[red, 0], Y[red, 1], c="r")
+    plt.scatter(Y[blue, 0], Y[blue, 1], c="b")
+    plt.scatter(Y[yellow, 0], Y[yellow, 1], c="y")
+    plt.scatter(Y[gray, 0], Y[gray, 1], c="gray")
+
+
+
+    plt.show()
+    plt.close()
+
+    # total_rec.get_population_vectors(0, 500)
+
+    # rec = Recording(paths.Ksdir, paths.SoundInfo, name='testing')
+    # rec.select_data_quality(quality='good')
+    # rec.ttl_alignment(multi=False)
+
+
+
+
+    # Still need to drax psycoM curves
+
+
+
+
+
+    # Compute global N x T matrix
+    # dict_timings = {}
+    # for clu in track(usable_clusters, description='Computing individual cluster ...'):
+    #   dict_timings[clu] = np.where(spikes_cluster == clu)[0]
+
+
+    # dict_stim_timings = {}
+    # for stim in track(np.unique(stim_vector), description='Generating individual timings ...'):
+    #   curr_stim = np.where(stim_vector == stim)[0]
+    #   dict_stim_timings[stim] = {}
+    #   for i, pres in enumerate(curr_stim):
+    #       ttl = ttl_indices[pres]
+    #       dict_stim_timings[stim][i] = [(v[np.logical_and(v >= ttl - params.pad_before, v <= ttl + params.pad_after)] - ttl)/params.fs*1000 for v in list(dict_timings.values())]
+
+    # for stim in dict_stim_timings:
+    #   curr_stim = [[sum(list(dict_stim_timings[stim][i][j])) for i in dict_stim_timings[stim]] for j in range(len(usable_clusters))]
+    #   dict_stim_timings[stim] = [a for l in curr_stim for a in l if a]
+    #   dict_stim_timings[stim].sort()
+    # #     dict_stim_timings[stim] = [l for l in curr_stim if l]
+
+    # print([len(dict_stim_timings[stim]) for stim in dict_stim_timings])
+
+
+
+
+
+
+
+
+    # all_psth = {}
+    # for i in track(range(max(stim_vector)), description='Generating PSTHs...'):
+    #   psths = []
+    #   curr_stim = np.where(stim_vector == i)[0]
+    #   for stim in curr_stim:
+    #       curr_ttl = list(ttl_indices)[0::2][stim]
+
+    #       curr_n_spikes = spikes_times[spikes_times > curr_ttl - params.pad_before]
+    #       curr_n_spikes = np.array(curr_n_spikes[curr_n_spikes < curr_ttl + params.pad_after], dtype=np.int64)
+    #       psths.append(np.array(curr_n_spikes - curr_ttl)/params.fs*1000)
+
+    #   psth = [i for p in psths for i in p]
+    #   all_psth[sound_names[i]] = psth
+
+
+
+    # for sound in track(all_psth, description='Drawing Figures...'):
+    #   sns.set_theme(style='ticks')
+
+    #   f, ax = plt.subplots(figsize=(7, 5))
+    #   sns.despine(f)
+
+    #   sns.histplot(data=all_psth[sound], palette='light:m_r',
+    #                edgecolor='.3', linewidth=.5, bins=50)
+    #   plt.axvline(0, color='red')
+    #   ax.set_xlabel('Time (ms)')
+    #   ax.set_ylabel('# of spikes')
+    #   ax.set_title('{}'.format(sound[:-4]))
+    #   plt.savefig(os.path.join(paths.Output, '{}.png'.format(sound[:-4])), dpi=150)
+    #   plt.close()
+
+
+    # May be useful to get correlation matrices between histograms
